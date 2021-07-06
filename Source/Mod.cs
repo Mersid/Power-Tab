8e4858e07d09c588efa73607183a676f45ad7eeb
Mersid <mersid_email@yahoo.com>
2021-07-06 16:08:37 -0700

    Harmony stuff. Took like two days, but we got something at least...

---
+++ Source/Mod.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Compilatron
+{
+	[StaticConstructorOnStartup]
+	public class Mod
+	{
+		static Mod() // Mod entrypoint
+		{
+			Harmony harmony = new Harmony("net.mersid.powertab");
+			harmony.PatchAll();
+		}
+	}
+
+	//[HarmonyPatch(typeof(Thing))]
+	//[HarmonyPatch("GetInspectTabs")]
+	[HarmonyPatch(typeof(MainTabWindow_Inspect))]
+	[HarmonyPatch(MethodType.Getter)]
+	[HarmonyPatch("CurTabs")]
+	public class Patch
+	{
+		private static void Postfix(InspectTabBase __instance, ref IEnumerable<InspectTabBase> __result)
+		{
+			Thing selectedThing = Find.Selector.SingleSelectedThing;
+
+			CompPower compPower = (selectedThing as ThingWithComps)?.TryGetComp<CompPower>();
+			if (compPower is null)
+				return;
+			
+			StringBuilder stringBuilder = new StringBuilder();
+			Log.Warning($"Connectors ({compPower.PowerNet.connectors.Count})");
+			foreach (CompPower x in compPower.PowerNet.connectors)
+			{
+				stringBuilder.Append(x + "\n");
+			}
+			Log.Message(stringBuilder.ToString());
+			stringBuilder.Clear();
+
+			Log.Warning($"Transmitters ({compPower.PowerNet.transmitters.Count})");
+			foreach (CompPower x in compPower.PowerNet.transmitters)
+			{
+				stringBuilder.Append(x + "\n");
+			}
+			Log.Message(stringBuilder.ToString());
+			stringBuilder.Clear();
+
+			Log.Warning($"Battery Comps ({compPower.PowerNet.batteryComps.Count})");
+			foreach (CompPowerBattery x in compPower.PowerNet.batteryComps)
+			{
+				stringBuilder.Append(x.parent.def.LabelCap + $" ({x.StoredEnergy}Wd)\n");
+			}
+			Log.Message(stringBuilder.ToString());
+			stringBuilder.Clear();
+			
+			Log.Warning($"Power Comps ({compPower.PowerNet.powerComps.Count})");
+			foreach (CompPowerTrader x in compPower.PowerNet.powerComps)
+			{
+				stringBuilder.Append(x.parent.def.LabelCap + $" ({x.GetType().BaseType}/{x.GetType().BaseType.BaseType}, {x.PowerOutput}W)\n");
+			}
+			Log.Message(stringBuilder.ToString());
+			stringBuilder.Clear();
+
+			__result = __result.AddItem(new PowerTab());
+		}
+	}
+}
